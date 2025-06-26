a6a6a3879f3428d6c15168b2e1144296618a8e59
splendidbrill <splendidbrill@gmail.com>
2025-06-26 17:02:00 +0200

    hero in hero2

--- src/app/components/Hero2.jsx
+++ src/app/components/Hero2.jsx
@@ -10,7 +10,7 @@ export const Hero2 = () => {
   return (
     <div>
       <Navbar />
-      {/* <main class="mt-16">
+      <main class="mt-16">
         <div id="hero" class="min-h-screen bg-gradient-to-b from-purple-50 via-orange-50 to-transparent">
            <div id="hero-container" class="max-w-4xl mx-auto px-6 pt-6 pb-16 flex flex-col sm:items-center sm:text-center sm:pt-12 sm:max-w-2xl">
                 <div id="version-text" class="flex items-center my-6 gap-2 border border-yellow-300 
@@ -36,7 +36,7 @@ export const Hero2 = () => {
                 <h1 class="text-4xl font-semibold leading-snug mt-4 sm:text-6xl">Web app to desktop app in minutes</h1>
                 <p class="text-xl mt-4 sm:text-2xl sm:mt-8 text-gray-800">Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
                 <div id="buttons-container" class="mt-12 flex gap-4 flex-col sm:flex-row">
-                    <button class="px-8 py-3 font-semibold rounded-lg text-white bg-primary shadow-sm hover:bg-opacity-90">Download now</button>
+                    <button class="px-8 py-3 font-semibold rounded-lg text-white bg-blue-800 shadow-sm hover:bg-opacity-90">Download now</button>
                     <button class="px-8 py-3 font-semibold rounded-lg bg-white border border-gray-400 hover:border-gray-800">Read Docs</button>
                 </div>
            </div>
@@ -44,7 +44,7 @@ export const Hero2 = () => {
                 
                 
         </div>
-      </main> */}
+      </main>
     </div>
   );
 };
