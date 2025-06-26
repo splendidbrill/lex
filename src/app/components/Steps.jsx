cda1120bb3c5194b8d70cf9ffe4e2c3bffeddfbf
splendidbrill <splendidbrill@gmail.com>
2025-06-26 20:31:00 +0200

    steps sections done

---
+++ src/app/components/Steps.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Steps = () => {
+  return (
+    <div id="steps" className="flex flex-col gap-6 mt-20  px-4 md:px-16 lg:px-32 ">
+            <h2 className="text-5xl sm:font-semibold mb-14">How it works</h2>
+            <div id="step1" className="rounded-xl border border-gray-200/70 px-8 py-12 flex flex-col lg:flex-row justify-between">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step 1</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <Image src="./assets/asset 66.svg" alt="" width={500} height={300}/>
+                </div>
+            </div>
+            <div id="step2" className="rounded-xl border border-gray-200/70 px-8 py-12 flex flex-col lg:flex-row justify-between">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step 2</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <Image src="./assets/asset 66.svg" alt="" width={500} height={300} />
+                </div>
+            </div>
+            <div id="step3" className="rounded-xl border border-gray-200/70 px-8 py-12 flex flex-col lg:flex-row justify-between">
+                <div className="flex flex-col gap-6 lg:w-1/2">
+                    <span className="border border-yellow-300 bg-yellow-50 text-yellow-800 w-fit px-3 py-1 rounded-lg font-medium font-display">Step 3</span>
+                    <h3 className="text-4xl">Bootstrap straight from your web app</h3>
+                    <p className="text-lg font-light">Copy and paste your web app url into ToDesktop. Customise your app design, app icon and window frame UI with no code.</p>
+                    <ul className="grid grid-cols-2 gap-2">
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                        <li className="flex gap-4">
+                            <i className="fa-solid fa-check text-gray-500"></i>
+                            <a className="text-lg font-light border-b border-black border-opacity-30 hover:border-opacity-80" href="">Multiple windows</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="pt-12">
+                    <Image src="./assets/asset 66.svg" alt="" width={500} height={300} />
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Steps
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
+import { Hero2 } from "./components/Hero2";
 
 export default function Home() {
  return (
   <div >
-    <Hero />
+    {/* <Hero /> */}
+    <Hero2 />
   </div>
  )
 }
