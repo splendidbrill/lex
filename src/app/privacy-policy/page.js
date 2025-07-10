2e0abba9f845731c249b9dcd724e5361ddcfdb6e
splendidbrill <splendidbrill@gmail.com>
2025-07-10 21:23:29 +0200

    removed about

--- src/app/privacy-policy/page.js
+++ src/app/privacy-policy/page.js
@@ -1,16 +1,25 @@
+// import React from 'react'
+
+// const page = () => {
+//   return (
+//     <div style={{ marginLeft: '20%', marginRight: '20%' }}>
+//       <h1>PRIVACY POLICY</h1>
+//       <p>Last updated June 10, 2025</p>
+//       <p>This Privacy Notice for KOLKAR TECHNOLOGIES INC. (doing business as Lex Bot) ("we," "us," or "our"), describes how and why we might access, collect, store, use, and/or share ("process") your personal information when you use our services ("Services"), including when you:</p>
+//       <ul>
+//         <li>Visit our website at https://www.lexlaunchcrew.com or any website of ours that links to this Privacy Notice</li>
+//         <li>Engage with us in other related ways, including any sales, marketing,</li>
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default page
 import React from 'react'
 
 const page = () => {
   return (
-    <div style={{ marginLeft: '20%', marginRight: '20%' }}>
-      <h1>PRIVACY POLICY</h1>
-      <p>Last updated June 10, 2025</p>
-      <p>This Privacy Notice for KOLKAR TECHNOLOGIES INC. (doing business as Lex Bot) ("we," "us," or "our"), describes how and why we might access, collect, store, use, and/or share ("process") your personal information when you use our services ("Services"), including when you:</p>
-      <ul>
-        <li>Visit our website at https://www.lexlaunchcrew.com or any website of ours that links to this Privacy Notice</li>
-        <li>Engage with us in other related ways, including any sales, marketing,</li>
-      </ul>
-    </div>
+    <div>page</div>
   )
 }
 
